a57841bc07fe3aecd897be98ca324599f2075d5b
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-02-20 01:11:43 -0500

    Initial Commit

---
+++ Shopapp/ShopappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopappApp.swift
+//  Shopapp
+//
+//  Created by Jairo Jair Toro Novellis on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ShopappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
